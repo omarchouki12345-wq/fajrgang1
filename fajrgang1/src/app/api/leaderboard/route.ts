3504b3e32935f8da3bf59444f1357835aa10e569
omarchouki12345-wq <omarchouki12345@gmail.com>
2026-09-15 00:54:25 +0200

    Delete fajrgang1 directory

--- fajrgang1/src/app/api/leaderboard/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getLeaderboard } from "@/lib/db";
-
-export async function GET() {
-  const leaderboard = await getLeaderboard();
-  return NextResponse.json({ leaderboard });
-}
